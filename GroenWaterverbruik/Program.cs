e846d99c07ea9227e1c2698c126cdb88ce41d39a
Arthur Schut <aschutgh@gmail.com>
2018-06-05 14:34:26 +0200

    Add project files.

---
+++ GroenWaterverbruik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GroenWaterverbruik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
